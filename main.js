830da0b90ed68e255abc6c1654457676284c29ff
fsdsfd <tomasmarcos2019@gmail.com>
2024-05-30 15:47:34 -0300

    Funcion de :hover a la imagen con sonido agregada

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './SASS/main.scss'
+document.querySelectorAll('.card__image').forEach(function(image) {
+    const audioId = image.getAttribute('data-audio');
+    const audioElement = document.getElementById(audioId);
+
+    image.addEventListener('mouseover', function() {
+        audioElement.play();
+    });
+
+    image.addEventListener('mouseout', function() {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+    });
+});
