36c39833d5f98dea9e0750e499272e4f1b03b0c2
fsdsfd <tomasmarcos2019@gmail.com>
2024-06-05 16:17:04 -0300

    carrousel agregado y mapa con fondo en contactos

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.querySelectorAll('.card__image').forEach(function(image) {
         audioElement.currentTime = 0;
     });
 });
-
+// BACKGROUND COLOR DEL INDEX CAMBIA DEPENDIENDO DE LA HORA EN LA QUE EL USUARIO INGRESE
 function ColorHora() {
     let hora = new Date().getHours()
     const elemento = document.querySelector('.body-index');
@@ -21,7 +21,7 @@ function ColorHora() {
     if (hora >= 9 && hora <= 12){
         gradiante = 'linear-gradient(0deg,#200000 0%, #c38327 99%)'
     }else if(hora >= 12 && hora <= 18){
-        gradiante = 'linear-gradient(0deg,#200000 0%, #4b7b9f 99%);'
+        gradiante = 'linear-gradient(0deg,#200000 0%, #4b7b9f 99%)'
     }else{
         gradiante = 'linear-gradient(0deg,#000000 0%, #1B2838 99%)'
     }
@@ -29,21 +29,52 @@ function ColorHora() {
 
 }
 window.onload = ColorHora;
-const carouselImages = document.querySelector('.carousel-images');
-const prevButton = document.querySelector('.carousel-prev');
-const nextButton = document.querySelector('.carousel-next');
-let counter = 0;
 
-nextButton.addEventListener('click', () => {
-  if (counter < 2) { // Cambiar 2 por el número total de imágenes - 1
-    counter++;
-    carouselImages.style.transform = `translateX(-${counter * 100 / 3}%)`; // Cambiar 3 por el número total de imágenes
-  }
+// CARROUSEL DE IMAGENES
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button-right');
+const prevButton = document.querySelector('.carousel-button-left');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+let currentIndex = 0;
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
 });
 
-prevButton.addEventListener('click', () => {
-  if (counter > 0) {
-    counter--;
-    carouselImages.style.transform = `translateX(-${counter * 100 / 3}%)`; // Cambiar 3 por el número total de imágenes
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  let nextSlide = currentSlide.nextElementSibling;
+  const nextIndex = (currentIndex + 1) % slides.length;
+
+  if (!nextSlide) {
+    nextSlide = slides[0]; // Vuelve a la primera diapositiva
   }
+
+  moveToSlide(track, currentSlide, nextSlide);
+  currentIndex = nextIndex;
 });
+
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  let prevSlide = currentSlide.previousElementSibling;
+  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
+
+  if (!prevSlide) {
+    prevSlide = slides[slides.length - 1]; // Vuelve a la última diapositiva
+  }
+
+  moveToSlide(track, currentSlide, prevSlide);
+  currentIndex = prevIndex;
+});
+
+slides[0].classList.add('current-slide');
